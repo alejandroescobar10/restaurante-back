28b65517009eb27403c94922bc555665f83f2faa
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2024-05-27 18:55:17 -0500

    segundo comentario

--- index.js
+++ index.js
@@ -16,7 +16,19 @@ const rutas = express.Router();//Se agrega al index de las rutas
 routerApi(rutas); 
 app.use('/v1', rutas);//Se agrega al index de las rutas
 
-
+app.get("/", (req, res) => {
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>NodeJs y Express en Vercel</title>
+        </head>
+        <body>
+          <h1>Soy un proyecto Back end en vercel</h1>
+        </body>
+      </html>
+    `;
+    res.send(htmlResponse);
+  });
 
 connectDB();
 app.listen(4000, ()=>{
